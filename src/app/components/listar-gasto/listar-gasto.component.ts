fcff35a75e661cd09dbdd6d3ccfb6b5c9765ea96
Gilbert Veitia Urbay <gveitia13@gmail.com>
2023-05-03 12:38:12 -0400

    video 92 end

--- src/app/components/listar-gasto/listar-gasto.component.ts
+++ src/app/components/listar-gasto/listar-gasto.component.ts
@@ -11,12 +11,15 @@ export class ListarGastoComponent implements OnDestroy, OnInit {
   subscription: Subscription
   presupuesto: number
   restante: number
+  listGastos: any[] = []
 
   constructor(private _presupuestoService: PresupuestoService) {
     this.presupuesto = 0
     this.restante = 0
     this.subscription = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data)
+      this.listGastos.push(data)
+      this.restante = this.restante - data.cantidad
     })
   }
 
@@ -27,5 +30,12 @@ export class ListarGastoComponent implements OnDestroy, OnInit {
   ngOnInit(): void {
     this.presupuesto = this._presupuestoService.presupuesto
     this.restante = this._presupuestoService.restante
+  }
+  aplicarColorrestante(){
+    if (this.presupuesto/4 > this.restante)
+      return'alert alert-danger'
+    else if (this.presupuesto/2>this.restante)
+      return 'alert alert-warning'
+    else return 'alert alert-secondary'
   }
 }
